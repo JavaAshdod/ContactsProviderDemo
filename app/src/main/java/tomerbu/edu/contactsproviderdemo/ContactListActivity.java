a7d59561ba8e88d58e20ed85a052e31343a9e410
javaAshdod <java.ashdod@gmail.com>
2016-08-09 14:33:09 +0300

    Contacts Provider Phones

--- app/src/main/java/tomerbu/edu/contactsproviderdemo/ContactListActivity.java
+++ app/src/main/java/tomerbu/edu/contactsproviderdemo/ContactListActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -65,10 +66,9 @@ public class ContactListActivity extends AppCompatActivity {
             }
         });
 
-        if (checkContactsPermission()){
+        if (checkContactsPermission()) {
             readContacts();
-        }
-        else {
+        } else {
             requestContactsPermission();
         }
 
@@ -86,18 +86,16 @@ public class ContactListActivity extends AppCompatActivity {
     }
 
 
-    private boolean checkContactsPermission(){
+    private boolean checkContactsPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if(checkSelfPermission(android.Manifest.permission.READ_CONTACTS)== PackageManager.PERMISSION_GRANTED){
+            if (checkSelfPermission(android.Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
                 //We have the permission
                 return true;
-            }
-            else {
+            } else {
                 //need to request the permission
                 return false;
             }
-        }
-        else {
+        } else {
             //We already have the permission:
             return true;
         }
@@ -105,8 +103,8 @@ public class ContactListActivity extends AppCompatActivity {
 
 
     @TargetApi(Build.VERSION_CODES.M)
-    private void requestContactsPermission(){
-        requestPermissions(new String[]{android.Manifest.permission.READ_CONTACTS }, REQUEST_CODE_CONTACTS);
+    private void requestContactsPermission() {
+        requestPermissions(new String[]{android.Manifest.permission.READ_CONTACTS}, REQUEST_CODE_CONTACTS);
     }
 
 
@@ -114,31 +112,48 @@ public class ContactListActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         //Get the result:
-        if (requestCode == REQUEST_CODE_CONTACTS){
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == REQUEST_CODE_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 readContacts();
-            }else {
-               Snackbar.make(fab, "We need your contacts to display the list", Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
-                   @Override
-                   public void onClick(View view) {
-                       requestContactsPermission();
-                   }
-               }).show();
+            } else {
+                Snackbar.make(fab, "We need your contacts to display the list", Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        requestContactsPermission();
+                    }
+                }).show();
             }
         }
     }
 
     //Method the requires permission:
     private void readContacts() {
-        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        Uri contactsURI = ContactsContract.Contacts.CONTENT_URI;
+
+        Cursor cursor = getContentResolver().query(contactsURI, null, null, null, null);
 
         if (cursor.moveToFirst()){
             do{
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                Toast.makeText(ContactListActivity.this, name + " " + id, Toast.LENGTH_SHORT).show();
 
+                String phone = getPhone(id);
             }
             while (cursor.moveToNext());
         }
     }
+
+    private String getPhone(String id) {
+        Uri contentUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = getContentResolver().query(contentUri, null, "_ID = ?", new String[]{id}, null);
+        if (cursor.moveToFirst()) {
+            String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            Toast.makeText(ContactListActivity.this, number, Toast.LENGTH_SHORT).show();
+        }
+        return null;
+    }
+
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
         MyContactsProvider provider = new MyContactsProvider();
@@ -209,7 +224,7 @@ public class ContactListActivity extends AppCompatActivity {
 
             @Override
             public String toString() {
-                return  tvContactName.getText().toString();
+                return tvContactName.getText().toString();
             }
         }
     }
