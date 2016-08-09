3e30c92328675a967f1ad3266ec572724a516d1e
javaAshdod <java.ashdod@gmail.com>
2016-08-09 11:26:10 +0300

    Added Permssions

--- app/src/main/java/tomerbu/edu/contactsproviderdemo/ContactListActivity.java
+++ app/src/main/java/tomerbu/edu/contactsproviderdemo/ContactListActivity.java
@@ -1,7 +1,10 @@
 package tomerbu.edu.contactsproviderdemo;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -13,12 +16,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import tomerbu.edu.contactsproviderdemo.contacts.MyContactsProvider;
 import tomerbu.edu.contactsproviderdemo.models.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.Manifest;
 
 /**
  * An activity representing a list of Contacts. This activity
@@ -30,11 +36,13 @@ import java.util.List;
  */
 public class ContactListActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_CONTACTS = 10;
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
      */
     private boolean mTwoPane;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +53,7 @@ public class ContactListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,6 +61,13 @@ public class ContactListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        if (checkContactsPermission()){
+            readContacts();
+        }
+        else {
+            requestContactsPermission();
+        }
 
         View recyclerView = findViewById(R.id.contact_list);
         assert recyclerView != null;
@@ -65,6 +80,54 @@ public class ContactListActivity extends AppCompatActivity {
             // activity should be in two-pane mode.
             mTwoPane = true;
         }
+    }
+
+
+    private boolean checkContactsPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(android.Manifest.permission.READ_CONTACTS)== PackageManager.PERMISSION_GRANTED){
+                //We have the permission
+                return true;
+            }
+            else {
+                //need to request the permission
+                return false;
+            }
+        }
+        else {
+            //We already have the permission:
+            return true;
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void requestContactsPermission(){
+        requestPermissions(new String[]{android.Manifest.permission.READ_CONTACTS }, REQUEST_CODE_CONTACTS);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //Get the result:
+        if (requestCode == REQUEST_CODE_CONTACTS){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                readContacts();
+            }else {
+               Snackbar.make(fab, "We need your contacts to display the list", Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+                       requestContactsPermission();
+                   }
+               }).show();
+            }
+        }
+    }
+
+    //Method the requires permission:
+    private void readContacts() {
+
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
